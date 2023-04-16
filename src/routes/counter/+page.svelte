6be69381d9cb4e9c46befb2de8e87d73a4a94894
brnt-toast <yoyo.beerme@gmail.com>
2023-04-16 17:46:44 -0400

    counter/: added button component

---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Button from '../../components/Button.svelte'
+    let count  = 0;
+
+    const increment = () => count += 1;
+    const decrement = () => count -= 1;
+</script>
+
+<p>{count}</p>
+<Button on:click={increment}>+</Button>
+<Button on:click={decrement}>-</Button>
