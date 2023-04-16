00d8392c4c1db8fa71cfb56420667d715deb3fc7
brnt-toast <yoyo.beerme@gmail.com>
2023-04-16 19:15:01 -0400

    todo/: creation of todo component

---
+++ src/routes/todo/Todo.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Button from "../../components/Button.svelte";
+</script>
+
+<div class="todo">
+    <span>sample todo</span>
+    <Button on:click>del</Button>
+</div>
+
+<style>
+     .todo {
+        background-color: lightgray;
+    }
+</style>
