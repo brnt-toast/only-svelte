4f58e9b2b0d5bba9b755e5d92c93596f221f8aab
brnt-toast <yoyo.beerme@gmail.com>
2023-04-16 17:47:15 -0400

    todo/: init

---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+    let count = 0
+    const yup = () => count += 1
+</script>
+
+
+
+
+<h1>Todo App</h1>
+
+<form >
+    <input type="text" placeholder="add todo">
+    <button on:click={yup} type="submit">+</button>
+</form>
+
+<div class="todo">
+    <span>sample todo</span>
+    <button>del</button>
+</div>
+
+<div>
+    <p>{`there are ${count} pending tasks`}</p>
+</div>
+
+
+
+<style>
+    .todo {
+        background-color: lightgray;
+    }
+</style>
